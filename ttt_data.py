604b216e2885cd2ec4ca7fcf27968d2cab091c9b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-08-18 12:15:06 +0200

    WIP

---
+++ ttt_data.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def get_training_data_2d():
+    return  np.array([[[[0,0,0],
+                        [0,0,0],
+                        [0,0,0]]],
+                      [[[0,0,1],
+                        [0,1,0],
+                        [0,0,1]]],
+                      [[[1,0,1],
+                        [0,1,0],
+                        [0,0,0]]],
+                      [[[0,0,0],
+                        [0,1,0],
+                        [1,0,1]]],
+                      [[[0,0,1],
+                        [0,1,0],
+                        [1,0,0]]],
+                      [[[1,0,0],
+                        [1,0,0],
+                        [1,0,0]]],
+                      [[[0,0,0],
+                        [1,1,1],
+                        [0,0,0]]],
+                      [[[0,1,0],
+                        [0,1,0],
+                        [0,1,0]]]], "float32")
+
+def get_target_data_2d():
+    return np.array([[[[0]]],[[[0]]],[[[0]]],[[[0]]], [[[1]]],[[[1]]],[[[1]]],[[[1]]]], "float32")
+
+def get_validation_data_2d():
+    return np.array([[[[0,0,0],
+                       [0,0,0],
+                       [0,0,0]]],
+                     [[[0,0,1],
+                       [0,1,0],
+                       [0,0,1]]],
+                     [[[1,0,0],
+                       [0,1,0],
+                       [0,0,1]]],
+                     [[[0,0,1],
+                       [0,0,1],
+                       [0,0,1]]]], "float32")
+
+def reshape_training_data_1d(arr):
+    return arr.reshape(8,9)
+
+def reshape_validation_data_1d(arr):
+    return arr.reshape(4,9)
+
+def reshape_target_data_1d(arr):
+    return arr.reshape(8,1)
