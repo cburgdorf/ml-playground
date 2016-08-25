0fd87069e8f65d555bc55ceec6f4515768ddaf11
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-08-25 23:22:18 +0200

    WIP

---
+++ questions.md
@@ -0,0 +1,1 @@
+## built in support for how to calculate how well our model performs on validation data? (accuracy)
--- ttt_keras.py
+++ ttt_keras.py
@@ -14,18 +14,18 @@ target_data = ttt_data.get_target_data_2d()
 validation_data = ttt_data.get_validation_data_2d()
 
 model = Sequential()
-model.add(Convolution2D(4, 2, 2, input_shape=(1, 6, 6), activation='relu'))
-model.add(Convolution2D(4, 2, 2, activation='relu'))
-model.add(Convolution2D(4, 2, 2, activation='relu'))
+model.add(Convolution2D(6, 2, 2, input_shape=(1, 6, 6), activation='relu'))
+model.add(Convolution2D(6, 2, 2, activation='relu'))
+model.add(Convolution2D(6, 2, 2, activation='relu'))
+model.add(Convolution2D(6, 2, 2, activation='relu'))
 model.add(Flatten())
 model.add(Dense(1, activation='sigmoid'))
+model.summary()
 
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='binary_crossentropy', optimizer=sgd)
+#sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 history = model.fit(training_data, target_data, nb_epoch=10000, batch_size=4, verbose=0)
+#print(history.history)
 
 print(model.predict(validation_data))
